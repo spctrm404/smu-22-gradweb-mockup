d60557b6f08e1f0efcaac36fc4f7051650639241
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2022-09-20 02:10:34 +0900

    new layout

--- js/home.js
+++ js/home.js
@@ -1,5 +1,15 @@
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    scrollUpBtn.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  }, 1);
+});
 const scrollUpBtn = document.querySelector('#scrollUpBtn');
 const menuBar = document.querySelector('#menuBar');
 scrollUpBtn.addEventListener('click', (e) => {
   menuBar.scrollIntoView({ behavior: 'smooth' });
+});
+const poster = document.querySelector('#poster');
+const homeBtn = document.querySelector('#homeBtn');
+homeBtn.addEventListener('click', (e) => {
+  poster.scrollIntoView({ behavior: 'smooth' });
 });
